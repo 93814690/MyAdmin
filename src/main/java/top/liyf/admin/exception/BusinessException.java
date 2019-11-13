49c3f0e1c4914b4f83ec1a0b9a602c3c909116bc
yunfei <93814690@qq.com>
2019-11-13 18:04:51 +0800

    自定义异常

---
+++ src/main/java/top/liyf/admin/exception/BusinessException.java
@@ -0,0 +1,31 @@
+package top.liyf.admin.exception;
+
+import top.liyf.admin.result.ResultCode;
+
+import java.io.Serializable;
+
+/**
+ * @author liyf
+ * Created in 2019-11-13
+ */
+public class BusinessException extends RuntimeException implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private int code;
+
+    private String msg;
+
+    public BusinessException(String msg) {
+        super(msg);
+        this.code = ResultCode.UNKNOWN_ERROR.val();
+        this.msg = msg;
+    }
+
+    public BusinessException(ResultCode code) {
+        super(code.msg());
+        this.code = code.val();
+        this.msg = code.msg();
+    }
+
+}
